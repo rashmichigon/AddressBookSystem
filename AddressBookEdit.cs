95594b09394094cadd4fb036f45d14f98514f35e
Rashmi <rashmichigon65@gmail.com>
2022-11-24 14:18:35 +0530

    AddressBookUC3

---
+++ AddressBookEdit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class AddressBookEdit
+    {
+        public void list(Action<Address> action)
+        {
+            Address.ForEach(action);
+        }
+
+        public Address find(string name)
+        {
+            Address addr = addresses.Find((a) => a.name == name);
+            return addr;
+        }
+
+    }
+}
