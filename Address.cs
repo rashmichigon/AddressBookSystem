8413f68269a973636e4bca78bfa2feb7e800d61a
Rashmi <rashmichigon65@gmail.com>
2022-11-24 13:39:38 +0530

    AddressBookSystem

---
+++ Address.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class Address
+    {
+        public string name;
+        public string address;
+        public int phone_no;
+        public string city;
+        public string state;
+        public string zipcode;
+        public string email_id;
+
+        public Address(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+            this.phone_no = phone_no;
+            this.city = city;
+            this.state = state;
+            this.zipcode = zipcode;
+            this.email_id = email_id;
+
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AddressBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+    }
+    }
+}
