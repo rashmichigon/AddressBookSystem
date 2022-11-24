61c59a7c7d50c2cab13056f43499bce6345e7fc2
Rashmi <rashmichigon65@gmail.com>
2022-11-24 14:07:02 +0530

    AddressBookUC2

---
+++ AddressBook.cs
@@ -0,0 +1,35 @@
+﻿using MySqlX.XDevAPI.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class AddressBook
+    {
+        List<Address> addresses;
+
+        public AddressBook()
+        {
+            addresses = new List<Address>();
+        }
+
+        public bool add(string name, string address)
+        {
+            Address addr = new Address(name, address);
+            Address result = find(name);
+        }
+
+        if (Result == null) 
+            {
+                   AddressBook.Add(addr);
+                    return true;
+               }
+        else 
+             {
+                     return false;
+             }
+  }
+}
---
+++ AddressBookAdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class AddressBookAdd
+    {
+        public bool add(string name, string address)
+        {
+            Address addr = new Address(name, address);
+            Address result = find(name);
+
+            if (result == null)
+            {
+                addresses.Add(addr);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    }
